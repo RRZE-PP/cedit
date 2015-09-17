313739a86a6177b9caa748af00f2862fd5ccade7
t.animal <ar79yxiw@cip.cs.fau.de>
2015-09-17 17:39:38 +0200

    add database bootstrap config

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,8 +5,9 @@ class BootStrap {
 
 	def init = { servletContext ->
 
-		new ModelGeneratingScript([label:"test", content:"process(){[testKey:'testVal']}"]).save(failOnError:true)
-		new ModelDisplayingTemplate([label:"test", content:'This is the content! Script \'${attr["script"]}\' returned: ${testKey}']).save(failOnError:true)
+		def foo = new ModelGeneratingScript([label:"test", content:"process(){[testKey:'testVal']}"])
+		foo.save(failOnError:true)
+		new ModelDisplayingTemplate([label:"test", content:'This is the content! Script \'${attr["script"]}\' returned: ${testKey}', defaultScript: foo]).save(failOnError:true)
 	}
 	def destroy = {
 	}
