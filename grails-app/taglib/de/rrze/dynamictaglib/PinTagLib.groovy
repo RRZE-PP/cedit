d412921731062ac84b256bb3933d61fc26d7ca69
t.animal <ar79yxiw@cip.cs.fau.de>
2015-10-30 16:48:40 +0100

    use DSLs in the taglib

--- grails-app/taglib/de/rrze/dynamictaglib/PinTagLib.groovy
+++ grails-app/taglib/de/rrze/dynamictaglib/PinTagLib.groovy
@@ -10,40 +10,77 @@ class PinTagLib {
 	def groovyPagesTemplateEngine
 
 	static cachedScriptEnvironments = [:]
+	static cachedDSLProviders = [:]
+	static cachedDSLProvidersPerScript = [:]
 
 	def templ = { attrs, body ->
 
+		//workaround a bug in gscripting by getting the default dsl once before setting our own
+		gscriptingService.getDslProvider("default")
+
 		def template = null
 		def script = null
+		def dsl = null
 		def availableParameters =  ["attrs": attrs, "body": body(), "params": params, "session": session]
 		def origData = ["attrs": attrs.clone(), "body": body(), "params": params.clone()]
 
+		//get the required objects
 		if("id" in attrs)
 			template = ModelDisplayingTemplate.get(attrs.id)
 		else
 			template = ModelDisplayingTemplate.findByLabel(attrs.label)
 
 		if("scriptId" in attrs)
-			script = ModelGeneratingScript.get(id)
+			script = ModelGeneratingScript.get(attrs.scriptId)
 		else if("scriptLabel" in attrs)
 			script = ModelGeneratingScript.findByLabel(attrs.scriptLabel)
 		else
 			script = template?.defaultScript
 
+		if("dslId" in attrs)
+			dsl = ScriptInterpretingDSL.get(attrs.dslId)
+		else if("dslLabel" in attrs)
+			dsl = ScriptInterpretingDSL.findByLabel(attrs.dslLabel)
+		else
+			dsl = script?.defaultDSL
+
+
 		//check for errors
-		if(!template){
-			out << "<!-- Error: unknown template! Could not render the element -->";
-			return
-		}
+		if(!template)
+			throw Exception("Unknown template label or id specified. Cannot render the element.");
+
+		if(!script && ("scriptId" in attrs || "scriptLabel" in attrs))
+			throw Exception("Unknown script label or id specified. Cannot create model for template.");
+
+		if(script && dsl == null && ("dslId" in attrs || "dslLabel" in attrs))
+			throw Exception("Unknown dsl label or id specified. Cannot create dsl for script.");
+
 
 		//create and store script environment and run script
 		if(script){
+			def dslProviderLabel = "default"
+
+			//instanciate a DSLProvider, if the script requires a special dsl
+			if(dsl != null){
+				dslProviderLabel = "de.rrze.dynamictaglib."+dsl.label
+
+				if(!(dslProviderLabel in cachedDSLProviders) || cachedDSLProviders[dslProviderLabel] < dsl.lastUpdated){
+					//possible lost update here!
+					cachedDSLProviders[dsl.label] = dsl.lastUpdated
+					gscriptingService.registerDslProvider(dslProviderLabel, new DynamicDSLProvider(dsl))
+				}
+			}
+
 			def envLabel = "de.rrze.dynamictaglib."+script.label
 
-			if(!(envLabel in cachedScriptEnvironments) || cachedScriptEnvironments[envLabel] < script.lastUpdated){
-				//possible lost update here!
-				cachedScriptEnvironments[envLabel] = script.lastUpdated
-				gscriptingService.registerScriptRuntimeEnv(envLabel, script.content)
+			if(!(envLabel in cachedScriptEnvironments)  //create if new environment
+				|| cachedScriptEnvironments[envLabel] < script.lastUpdated //or if the script was updated in the db
+				|| cachedDSLProvidersPerScript[envLabel] != dslProviderLabel //or if the dsl provider for this script has changed
+				){
+					//possible lost update here!
+					cachedScriptEnvironments[envLabel] = script.lastUpdated
+					cachedDSLProvidersPerScript[envLabel] = dslProviderLabel
+					gscriptingService.registerScriptRuntimeEnv(envLabel, script.content, dslProviderLabel)
 			}
 
 			def model = gscriptingService.run(envLabel, availableParameters)
@@ -52,9 +89,12 @@ class PinTagLib {
 				availableParameters.putAll(model)
 		}
 
+
 		//store original data (unchanged by script, guaranteed)
 		availableParameters["origData"] = origData
 
+
+		//write the output
 		def resultSW = new StringWriter()
 		def result = ""
 		groovyPagesTemplateEngine
