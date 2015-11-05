e8d01c0390f28754a2852e1e7c32107271250890
t.animal <ar79yxiw@cip.cs.fau.de>
2015-11-05 18:48:54 +0100

    add a simple dsl

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,73 +7,46 @@ class BootStrap {
 	def init = { servletContext ->
 
 		def dsl = new ScriptInterpretingDSL([label:"testDSL", handler: "dashboard", code:'''
-import java.text.SimpleDateFormat
-import java.util.Date;
+class SimpleDSL implements GroovyInterceptable {
 
-import org.apache.commons.logging.LogFactory
+	Map params
 
-import grails.plugin.gscripting.ScriptRuntimeEnv;
-import grails.plugin.gscripting.dsl.IContext;
-import grails.util.Holders
-
-
-class DashboardDslInterceptor implements GroovyInterceptable {
-
-	def grailsApplication
-	def script
-	def log
-	def app = [:]
-
-	Map scriptParams
-	ScriptRuntimeEnv sre
-
-	DashboardDslInterceptor(ScriptRuntimeEnv sre) {
-		this.grailsApplication = Holders.getGrailsApplication()
-		this.script = sre?.additionalData
-		this.scriptParams = scriptParams
-		this.sre = sre
-
-		this.grailsApplication.serviceClasses.each {
-			def bean = this.grailsApplication.mainContext.getBean(it.propertyName)
-			this.app.put(it.propertyName, bean)
-		}
-
-		this.log = LogFactory.getLog("de.rrze.dynamictaglib.${script?.label?.capitalize()}.${script?.id}".toString());
-	}
-
-	public Object invokeMethod(String name, Object args) {
-		try{
-			def calledMethod = DashboardDslInterceptor.metaClass.getMetaMethod(name, args)
-			return calledMethod?.invoke(this, args)
-		}
-		catch(Throwable t){
-			t.printStackTrace();
-			throw t
-		}
+	SimpleDSL(Map callParams) {
+		this.params = callParams
 	}
 
 	public Object getProperty(String name) {
-		if(name in DashboardDslInterceptor.metaClass.properties.name)
-			return  DashboardDslInterceptor.metaClass.getProperty(this, name)
+      if(this.params.containsKey(name)){
+      	return this.params[name]
+      }
 
-		throw new MissingPropertyException(name,this.class)
-	}
-
-	void setProperty(String name, args) {
-		if(name in DashboardDslInterceptor.metaClass.properties.name)
-			DashboardDslInterceptor.metaClass.setProperty(this, name, args)
+      throw new MissingPropertyException(name, this.getClass())
 	}
 }
 
-process(dslDefinition:'dashboard')
+process()
 {
-   new DashboardDslInterceptor(__dsl__script)
+   new SimpleDSL(ctx.callParams)
 }
 '''
 			]).save(failOnError:true)
 		def foo = new ModelGeneratingScript([label:"test", content:"process(){[testKey:'testVal']}"])
 		foo.save(failOnError:true)
-		new ModelDisplayingTemplate([label:"test", content:'This is the content! Script \'${attrs.scriptLabel?attrs.scriptLabel:"defaultScript"}\' returned: ${testKey}', defaultScript: foo]).save(failOnError:true)
+		new ModelDisplayingTemplate([label:"test", content:"""
+This is the template content! The script '\${meta.scriptLabel}'
+
+<g:if test="\${meta.scriptFailed}">
+	failed
+	<g:if test="\${'scriptException' in meta}">
+    	due to this exception: <br />
+        <pre>
+        	\${meta.scriptException}
+        </pre>
+    </g:if>
+</g:if>
+<g:else>
+	returned: \${data}
+</g:else>""", defaultScript: foo]).save(failOnError:true)
 	}
 	def destroy = {
 	}
